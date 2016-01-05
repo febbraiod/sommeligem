b1725567f6ba3f132c22695572a4be6a3e809338
febbraiod <donedesign1@gmail.com>
2016-01-05 14:21:20 -0500

    flesh out interface method in Som_cli

--- lib/sommeligem/som_cli.rb
+++ lib/sommeligem/som_cli.rb
@@ -42,20 +42,34 @@ class Som_cli
     puts "How many I assist you? If you are unfamilar with Sommeligem, please enter 'help'."
     
     guest_choice = gets.chomp
-    if guest_choice.downcase == 'help'
-      puts ""
-      sleep(1.to_f)
-      puts "You may choose a number 1-100 for more details on the wine of your choice."
-      sleep(3/2.to_f)
-      puts "You may also repeat this list by typing 'list'."
-      sleep(3/2.to_f)
-      puts "Or if you'd like, I could suggest wines from our list to pair with your meal this evening by typing 'pairing'."
-      sleep(2.to_f)
-      puts "If you no longer need my assitance you may type 'exit.'"
-      puts ""
-      sleep(3/4.to_f)
-      interface
-    end
+      if guest_choice.downcase == 'help'
+        puts ""
+        sleep(1.to_f)
+        puts "You may choose a number 1-100 for more details on the wine of your choice."
+        sleep(3/2.to_f)
+        puts "You may also repeat this list by typing 'list'."
+        sleep(3/2.to_f)
+        puts "Or if you'd like, I could suggest wines from our list to pair with your meal this evening by typing 'pairing'."
+        sleep(2.to_f)
+        puts "If you no longer need my assitance you may type 'exit.'"
+        puts ""
+        sleep(3/4.to_f)
+        interface
+      elsif (1..100).include?(guest_choice.to_i)
+        #write details method and call it here
+      elsif guest_choice.downcase == 'list'
+        self.wine_list
+        interface
+      elsif guest_choice.downcase == 'pairing'
+        #write pairing method and call it here
+      elsif guest_choice.downcase == 'exit'
+        puts "Please come again!"
+        return 
+      else
+        puts "Im sorry I do not understand you request, please try again"
+        interface
+      end
+  end
 
     #Launchy.open("details link")
 
