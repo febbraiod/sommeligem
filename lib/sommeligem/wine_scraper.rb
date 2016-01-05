ced445fb71a9c1d26f50b6e25600f3cb7eaff0f4
febbraiod <donedesign1@gmail.com>
2016-01-04 21:07:49 -0500

    clean up Wine class, and add info to gemspec

--- lib/sommeligem/wine_scraper.rb
+++ lib/sommeligem/wine_scraper.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
@@ -11,9 +10,11 @@ class Wine
 
   attr_accessor :wine_url
 
+
   def initialize(wine_url = WINE_URL)
     @wine_url = WINE_URL
   end
+
 
   def scrape
 
@@ -25,8 +26,6 @@ class Wine
     regions = doc.css(".productAbstract").css("span + span [href]")
     varietals = doc.css("div.productAbstract").css("span:first-child [href]")
 
-
-
     idx = 1
     names_and_links.each do |wine|
       wine_hash = {}
@@ -36,7 +35,6 @@ class Wine
       @@wines << wine_hash
       idx += 1 #for ranking
     end
-
 
     idx2 = 0
     prices.each do |price|
@@ -58,7 +56,6 @@ class Wine
     
     idx4 = 0
     varietals.each do |grape|
-
       @@wines[idx4][:varietal] = grape.text.chomp("s")
       idx4 += 1
     end
@@ -71,21 +68,12 @@ class Wine
     @@wines
   end
 
+
+
 end
 
 puts(Wine.new.scrape)
 
-=begin
-[:2 => {:name => "Black Box", :varietal => "Red Blend",
-:detail_link_link => link, :price => $5,}, :3 => {}]
-=end
-
-#ranks = having trouble with this, however I might not need to scapre this.  it's really just 1-100 based on the "name"
-#names = doc.css(".listProductName")   #text   #pull this into an array and rank based on order.
-#varietals = doc.css(".productAbstract").css("[href]")  #text
-#detail_link => link
-#detail_link = doc.css(".listProductName").attribute("href").value
-#where grown = doc.css(".productAbstract").css("span + span [href]").first.text
 
 
 
--- sommeligem.gemspec
+++ sommeligem.gemspec
@@ -9,9 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["febbraiod"]
   spec.email         = ["donedesign1@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{a CLI gem that provides information about the top 100 wines on Wine.com including suggested pairings}
+  spec.homepage      = "https://github.com/febbraiod/sommeligem.git"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
