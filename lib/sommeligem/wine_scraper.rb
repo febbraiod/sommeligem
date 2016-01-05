1704fd688db3927f0babca64852393a11c038c07
febbraiod <donedesign1@gmail.com>
2016-01-04 19:59:01 -0500

    expand scraper, css keys found, problems with info labeling on source page

--- lib/sommeligem/wine_scraper.rb
+++ lib/sommeligem/wine_scraper.rb
@@ -7,9 +7,11 @@ class Wine
 
   WINE_URL = "http://www.wine.com/v6/winemarketinglist.aspx?N=490%201251&pagelength=100&s=100&cid=100"
 
+  @@wines = []
+
   attr_accessor :wine_url
 
-  def initialize(wine_rul = WINE_URL)
+  def initialize(wine_url = WINE_URL)
     @wine_url = WINE_URL
   end
 
@@ -18,34 +20,67 @@ class Wine
     html = Nokogiri::HTML(open(wine_url))
     doc = html.css(".detailsBlock")
     
-    names = doc.css(".listProductName")
+    names_and_links = doc.css(".listProductName")
+    prices = html.css(".priceBoxHeader")
+    varietals = doc.css(".productAbstract").css("[href]")
+    regions = doc.css(".productAbstract").css("span + span [href]").first.text
 
-    names.each do |i|
-     i.attribute("href").value
-     i.text
-     binding.pry
 
+
+    idx = 1
+    names_and_links.each do |wine|
+      wine_hash = {}
+      wine_hash[:ranking] = idx
+      wine_hash[:name] = wine.text
+      wine_hash[:detail_link] = wine.attribute("href").value
+      @@wines << wine_hash
+      idx += 1 #for ranking
     end
 
 
+    idx2 = 0
+    prices.each do |price|
+      if price.css(".soldOutPrice").text != ""
+        @@wines[idx2][:price] = price.css(".soldOutPrice").text
+      elsif price.css(".salesPrice").text != ""
+        @@wines[idx2][:price] = price.css(".salesPrice").text
+      else
+        @@wines[idx2][:price] = "We're sorry. Price not available for this wine. Please check the detail link for more information."
+      end
+      idx2 += 1
+    end
+
+    binding.pry
+
+    idx3 = 0
+    varietals.each do |grape|
+      @@wines[idx3][:varietal] = grape.text
+      idx3 += 1
+      binding.pry
+    end
 
   end
 
+
+  def self.all
+      @@wines
+  end
 
 end
 
 Wine.new.scrape
 
 =begin
-[{:rank => 2, :name => "Black Box", :varietal => "Red Blend" 
-  :details => {:price => $5, :rating => 80, :vinatage => 2014}},{}]
+[:2 => {:name => "Black Box", :varietal => "Red Blend",
+:detail_link_link => link, :price => $5,}, :3 => {}]
 =end
 
 #ranks = having trouble with this, however I might not need to scapre this.  it's really just 1-100 based on the "name"
 #names = doc.css(".listProductName")   #text   #pull this into an array and rank based on order.
 #varietals = doc.css(".productAbstract").css("[href]")  #text
-#description => link
-#description = doc.css(".listProductName").attribute("href").value
+#detail_link => link
+#detail_link = doc.css(".listProductName").attribute("href").value
+#where grown = doc.css(".productAbstract").css("span + span [href]").first.text
 
 
 
